d510a52bce5205775848408716096be12f49ecfb
jialihan <jiali.hjl@alibaba-inc.com>
2017-07-03 16:55:59 +0800

    source code added

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+
+functions:
+
+1. upload a single file
+2. download a single file
+3. upload multiple files
+
+framework£º springboot + view(html or thymeleaf)
---
+++ src/main/java/com/xiaojia/springboot/FiledemoApplication.java
@@ -0,0 +1,12 @@
+package com.xiaojia.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiledemoApplication.class, args);
+	}
+}
